d4f5859e9ec1fbf73e5669c006c8bf3fcf3ea916
Michal287 <michal.banach287@gmail.com>
2022-03-25 20:04:18 +0100

    push_2

--- lesson_3.py
+++ lesson_3.py
@@ -60,4 +60,3 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random
 rf = RandomForestRegressor(n_estimators=500, random_state=42)
 rf.fit(X_train, y_train)
 rf.score(X_test, y_test)
-

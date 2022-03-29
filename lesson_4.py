8f6698a48d67e17a5bee12636c90fea75fabd4f8
Michal287 <michal.banach287@gmail.com>
2022-03-29 22:06:13 +0200

    modified lesson_4

--- lesson_4.py
+++ lesson_4.py
@@ -1,105 +1,115 @@
 import numpy as np
 import pandas as pd
 from typing import List, Dict
-from scipy.stats import zscore as zscore_outlier, median_abs_deviation
-
-
-def read_file(path:str, cols:List[str]=None, col_types:Dict[str, str]=None) -> pd.DataFrame:
-    if cols is not None and col_types is not None:
-        df = pd.read_csv(path, usecols=cols, dtype = col_types)
-        return df.select_dtypes(include=['int16', 'int32', 'int64', 'float16', 'float32', 'float64'])
-        
-    else:
-        df = pd.read_csv(path, usecols=cols, dtype = col_types)
-        return df.select_dtypes(include=['int16', 'int32', 'int64', 'float16', 'float32', 'float64'])
-    
-    
-col_name = ['Price', 'Rooms', 'Car']
-
-col_type = {
-    
-    'Price': 'float64',
-    'Rooms': 'int16',
-    'Car': 'float64'
-}
-
-df = read_file('houses_data.csv', cols=col_name, col_types=col_type)
-df.head()
-df.isnull().sum()
-
-
-df.describe()
-
-
 from sklearn.impute import KNNImputer
-
-impt = KNNImputer()
-impt.fit(df[['Car']])
-df['Car'] = impt.transform(df[['Car']])
-df.hist()
-df.isnull().sum()
+from scipy.stats import zscore as zscore_outlier, median_abs_deviation
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 
 
-df[['Price']].boxplot()
+def read_file(path: str, cols: List[str] = None, col_types: Dict[str, str] = None,
+              num_only: bool = False) -> pd.DataFrame:
+    df = pd.read_csv(path, usecols=cols, dtype=col_types)
 
-df[['Rooms']].boxplot()
+    if num_only:
+        return df.select_dtypes(include=['int16', 'int32', 'int64', 'float16', 'float32', 'float64'])
 
-df[['Car']].boxplot()
-
-
-df_iqr_outliers = df.copy() 
-
-
-def iqr_outliers(df):
-    for x in df:
-        q90,q10 = np.percentile(df.loc[:,x],[90,10])
-        intr_qr = q90-q10
-
-        max = q90+(1.5*intr_qr)
-        min = q10-(1.5*intr_qr)
-
-        df.loc[df[x] < min,x] = np.nan
-        df.loc[df[x] > max,x] = np.nan
-
-  
-iqr_outliers(df_iqr_outliers)
-
-df_iqr_outliers.isnull().sum()
-
-df_iqr_outliers = df_iqr_outliers.dropna(axis = 0)
-
-df_iqr_outliers.isnull().sum()
+    return df
 
 
-def modified_z_score_outlier(column):
+def percentiles_outliers(column: pd.DataFrame) -> bool:
+    low, high = np.percentile(column, [10, 90])
+    return (column < low) | (column > high)
+
+
+def iqr_outliers(column: pd.DataFrame) -> bool:
+    Q1, Q3 = np.percentile(column, [10, 90])
+    IQR = Q3 - Q1
+    return (column < (Q1 - 1.5 * IQR)) | (column > (Q3 + 1.5 * IQR))
+
+
+def z_score(column: pd.DataFrame) -> bool:
+    return zscore_outlier(column) > 3
+
+
+def modified_z_score_outlier(column: pd.DataFrame) -> bool:
     mad_column = median_abs_deviation(column)
     median = np.median(column)
     mad_score = np.abs(0.6745 * (column - median) / mad_column)
     return mad_score > 3.5
 
 
-outliers_methods_dict = {
-    "z_score": zscore_outlier,
-    "mod_z_score": modified_z_score_outlier
+def log_transform(column: pd.DataFrame) -> bool:
+    return column.map(lambda x: 0 if x == 0 else np.log10(x)) == None
+
+
+def score_dataset(X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.DataFrame, y_test: pd.DataFrame) -> int:
+    regr_model = LinearRegression()
+    regr_model.fit(X_train, y_train)
+    preds = regr_model.predict(X_test)
+    return mean_absolute_error(y_test, preds)
+
+
+def knn_imputer(df: pd.DataFrame) -> pd.DataFrame:
+    knn = KNNImputer()
+    knn.fit(df)
+    return knn.transform(df)
+
+
+col_name = ['Price', 'Rooms', 'Car']
+
+col_type = {
+
+    'Price': 'float64',
+    'Rooms': 'int16',
+    'Car': 'float64'
 }
 
+df = read_file('houses_data.csv', cols=col_name, col_types=col_type, num_only=False)
+
+df.isnull().sum()
+
+df.describe()
+
+
+df[['Car']] = knn_imputer(df[['Car']])
+df.hist()
+df.isnull().sum()
+
+df[['Rooms']].boxplot()
+
+df[['Price']].boxplot()
+
+df[['Car']].boxplot()
+
+
+outliers_methods_dict = {
+    "log_transform": log_transform,
+    "percentiles_outliers": percentiles_outliers,
+    "iqr_outliers": iqr_outliers,
+    "z_score": z_score,
+    "modified_z_score": modified_z_score_outlier
+}
+
+df_copied = df.copy()
+
+print(1)
 
 for method_name, method in outliers_methods_dict.items():
     print("\nRunning method: ", method_name)
-    print(df.apply(lambda x: method(x)).sum())
 
+    df = df_copied.copy()
 
-def df_log(df):
-    columns = df.columns
+    print('\nOutliers:\n\n', df.apply(lambda x: method(x)).sum(), '\n\n')
 
-    for col in columns:
-        df[col] = np.log(df[col])
+    df = df[df.apply(lambda x: ~method(x))].dropna()
 
-    return df
+    print(df)
 
+    X = df[['Car', 'Rooms']]
+    y = df[['Price']]
 
-df_log = df_log(df)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-
-
-
+    print("\nMAE: ", score_dataset(X_train, X_test, y_train, y_test), "\n\n\n\n")
